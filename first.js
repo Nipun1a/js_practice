37424186a0bb041e2e91c9a1f62a130bc83cfad4
nipun <nipun1a1b@gmail.com>
2025-01-19 13:42:11 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,25 @@
+//console.log("welcome to the new world ");
+console.log("tera bhai seedhe maut");
+console.log("11k");
+let  radius =14;
+console.log (radius) ;
+const student = {
+    fullName :  "Rahul",
+    age  :  20 ,
+    cgpa :   9.5 ,
+    hobbies :  ["cricket" , "football" , "reading"],
+    isPass :  true,
+};
+console.log(student);
+let nipun= "champ";
+console.log (nipun);
+let islogin = true;
+let raddi=radius;
+console.log(raddi);
+let website="www.google.com";
+
+//objects;
+let student1=["happy", "sad", "angry"];
+let stemotions=[st_name:"nipun",emotion:student]
+console.log(stemotions);
+
