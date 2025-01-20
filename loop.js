c4e3fc41e00ed1faf6241678229d9f44acbd657d
nipun <nipun1a1b@gmail.com>
2025-01-20 22:41:17 +0530

    2nd_day_of_practice

--- loop.js
+++ loop.js
@@ -49,12 +49,41 @@ for (let index = 0; index < numbers.length; index++) {
 }
 console.log(multipliedno);*/
 
-let cities =["paris","ny","tokyo","london"];
+/*let cities =["paris","ny","tokyo","london"];
 let citylist=[];
 for (let index = 0; index < cities.length; index++) {
     const element = cities[index];
     citylist.push(element);
     
 }
-console.log(citylist);
+console.log(citylist);*/
+
+/*let tea = ["green tea","black tea","chai","oolong tea"];
+let element =[];
+for (let index = 0; index < tea.length; index++) {
+    if (tea[index] == "chai"){
+        continue; //break for the previous question;
+    }
+     element.push(tea[index]);
+    
+}
+console.log(element);*/
+
+/*let array = [1,2,3,4,5];
+let smallno = [];
+for (const num of array) {
+    if (num === 4){
+        continue; // break for the previous question
+    }
+    smallno.push(num);
+
+}
+console.log(smallno);*/
+
+
+let citiespopulation = {
+    "london": 8900000
+}
+
+
 
