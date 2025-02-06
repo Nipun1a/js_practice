10d12587dcf3b9fa6414c03b86c59dc9073d1448
nipun <nipun1a1b@gmail.com>
2025-02-06 10:10:41 +0530

    dom

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("hello from nipun");
+alert("connected");
+//example 1
+document.getElementById("btn").
+addEventListener('click', function (){
+    let para = document.getElementById("pa");
+    para.textContent= "the paragraph is change";
+    
+});
+
+// traversing the DOM 
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("myProjects").addEventListener("click", function () {
+        let subjects = document.querySelectorAll(".subject");
+        subjects.forEach(subject => {
+            subject.classList.add("highlight");
+        });
+    });
+});
+
+
+// example 3 
